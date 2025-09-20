72a58be4ea2a21a8c591d89830e35067de845477
VADYM DZIUBA <valerazabivay01@gmail.com>
2025-09-20 17:00:44 +0300

    Refactor scenes and prefabs, remove unused scripts
    Renamed and updated prefabs for food and snake tail, and removed unused or redundant scripts and scenes (2DScene, Menu). Updated Game.unity and project settings to reflect these changes. Added new sprite asset and reorganized .idea project files.

--- Assets/Scripts/FoodController.cs
+++ Assets/Scripts/FoodController.cs
@@ -1,15 +1,15 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodController : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D col)
-    {   // check if the food touches any part of the snake
-        // the tag "Snake" should be on the tail and head prefabs
-        if (col.gameObject.tag == "Snake")
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Snake"))
         {
-            Destroy(gameObject); // if yes, destroy the food
+            Destroy(gameObject);
         }
     }
 }
