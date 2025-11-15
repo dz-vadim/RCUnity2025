c7b646190f49f5a8f0d886f5c705659a6947eb7f
VADYM DZIUBA <valerazabivay01@gmail.com>
2025-11-15 17:55:23 +0200

    Add CapsuleController and update Cube movement logic
    Added a new CapsuleController script and associated GameObject to the scene. Updated CubeController to implement movement using Rigidbody velocity, and refactored movement methods. Adjusted scene objects and transforms to accommodate these changes.

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-
 public class CubeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
+        MoveWithRigidbodyVelocity();
+    }
+    private void MoveWithTransformPosition()
+    {
+        Vector3 position = transform.position;
+        if (Input.GetKey(KeyCode.W)) position.z += 0.01f;
+        if (Input.GetKey(KeyCode.S)) position.z -= 0.01f;
+        if (Input.GetKey(KeyCode.A)) position.x += 0.01f;
+        if (Input.GetKey(KeyCode.D)) position.x -= 0.01f;
         
+        if (Input.GetKey(KeyCode.Space)) position.y += 0.1f;
+        transform.position = position;
+    } 
+
+    private void MoveWithTransformTranslate()
+    {
+        float speed = 5f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.fixedDeltaTime  * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.fixedDeltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.fixedDeltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.fixedDeltaTime * speed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveWithRigidbodyVelocity()
     {
-        
+        float speed = 5f;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(moveX * speed, rb.velocity.y, moveZ * speed);
     }
 }
