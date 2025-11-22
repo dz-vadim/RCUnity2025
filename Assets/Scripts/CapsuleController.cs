9791c488fec0244b89f595ac3a3d455e4d400654
VADYM DZIUBA <valerazabivay01@gmail.com>
2025-11-22 17:19:52 +0200

    Add weapon pack, materials, and prefabs to project
    Imported Cartoon Weapon Pack assets, including FBX models, textures, and materials. Added new prefabs for Cube and Column, updated CapsuleController script, and included new scene files and lighting settings for MainScene and SampleScene.

--- Assets/Scripts/CapsuleController.cs
+++ Assets/Scripts/CapsuleController.cs
@@ -2,5 +2,37 @@ using UnityEngine;
 
 public class CapsuleController : MonoBehaviour
 {
-    
+    private const float SPEED = 4f;
+    private float moveX,  moveZ, moveY;
+    private CharacterController controller;
+    private Vector3 moveDirection;
+
+    private void Start()
+    {
+        moveY = 0;
+        controller = GetComponent<CharacterController>();
+    }
+    private void MoveWithCharacterController()
+    {
+        moveX = Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+        moveZ = Input.GetAxis("Vertical") * Time.fixedDeltaTime;
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveY = 0.15f;
+            }
+        }
+        else
+        {
+            moveY -= 1f * Time.fixedDeltaTime;
+        }
+        moveDirection = new Vector3(moveX, moveY, moveZ);
+        controller.Move(moveDirection * SPEED);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveWithCharacterController();
+    }
 }
