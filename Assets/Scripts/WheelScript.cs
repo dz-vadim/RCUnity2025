026ec76d7eba1b3bd63c00a5d2c3cb8419e41ac3
VADYM DZIUBA <dziuba.vadym@gmail.com>
2026-04-18 18:12:27 +0300

    comm

---
+++ Assets/Scripts/WheelScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelScript : MonoBehaviour
+{
+    [SerializeField] private Transform wheelTransform;
+    [SerializeField] private bool isSteer;
+    [SerializeField] private bool isInvertSteer;
+    [SerializeField] private bool isPower;
+    private float _steerAngle;
+    private float _motorTorque;
+    private WheelCollider _wheelCollider;
+    
+    void Start()
+    {
+        _wheelCollider = GetComponent<WheelCollider>();
+    }
+
+    void Update()
+    {
+        _wheelCollider.GetWorldPose(out Vector3 position, out Quaternion rotation);
+        wheelTransform.position = position;
+        wheelTransform.rotation = rotation * Quaternion.Euler(0, -90f, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isSteer)
+        {
+            _wheelCollider.steerAngle = _steerAngle * (isInvertSteer ? -1 : 1);
+        }
+
+        if (isPower)
+        {
+            _wheelCollider.motorTorque = _motorTorque;
+        }
+    }
+    
+    public void ChangeMotorTorque(float torque)
+    {
+        _motorTorque = torque;
+    }
+
+    public void ChangeSteerAngle(float angle)
+    {
+        _steerAngle = angle;
+    }
+}
