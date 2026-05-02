ab0d1ce146c1004c54b24ca8d2d719ad397c450e
VADYM DZIUBA <dziuba.vadym@gmail.com>
2026-05-02 15:57:58 +0300

    05.02.2026

---
+++ Assets/Valentines/Script/HeartSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject heart;
+    private void Start()
+    {
+        if(Random.Range(0,100) > 50)
+        {
+            Destroy(heart);
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-4f, 0f);
+    }
+}
---
+++ Assets/Valentines/Script/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject wall;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 0.5f, 2.5f);
+    }
+
+    private void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(0f, Random.Range(-2.5f, 2.5f), 0f); 
+        Destroy(Instantiate(wall, transform.position + spawnPosition, Quaternion.identity),10f);
+    }
+}
